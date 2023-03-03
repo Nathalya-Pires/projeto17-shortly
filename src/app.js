be4d4cbbd4084a6008206edc09f9d207b6d7ce45
Nathalya Ohana Pires Ferreira <nathyohana@gmail.com>
2023-03-03 15:09:36 -0400

    feat: corrigindo erros

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
+import urlRouter from "./routes/urlShortly.routes.js";
 
 
 dotenv.config();
@@ -10,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use([authRoutes]);
+app.use([authRoutes, urlRouter]);
 
 const port = process.env.PORT;
 app.listen(port, () => console.log(`Servidor rodando na porta: ${port}`));
