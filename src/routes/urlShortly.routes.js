2c6ebbd4ab7bbd67cae949c8c7c4e5b9037241a4
Nathalya Ohana Pires Ferreira <nathyohana@gmail.com>
2023-03-03 15:04:18 -0400

    feat: routes url ok

--- src/routes/urlShortly.routes.js
+++ src/routes/urlShortly.routes.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { validateSchema } from "../middlewares/validateSchema.js";
+import urlShortlySchema from "../schemas/urlShortlySchema.js"
+import {authValidation} from "../middlewares/auth.middleware.js"
+import { deleteUrl, openUrl, shortlyUrl, UrlById } from "../controllers/urlShortly.controllers.js";
 
 const urlRouter = Router();
 
-urlRouter.post("/urls/shorten", validateSchema());
-urlRouter.get("/urls/:id");
-urlRouter.get("/urls/open/:shortUrl");
-urlRouter.delete("/urls/:id", validateSchema());
+urlRouter.post("/urls/shorten", validateSchema(urlShortlySchema), authValidation, shortlyUrl);
+urlRouter.get("/urls/:id", UrlById);
+urlRouter.get("/urls/open/:shortUrl", openUrl);
+urlRouter.delete("/urls/:id", authValidation, deleteUrl);
 
 export default urlRouter;
